65b97b912ea12005a6f9fe3bc89f9e636d467ed8
SRIME <srikarthi01234@gmail.com>
2022-03-06 20:33:21 +0530

    .

---
+++ Leetcode/palindromeNumber.java
@@ -0,0 +1,8 @@
+/**Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+
+Solve it without converting the integer to string.*/
+
